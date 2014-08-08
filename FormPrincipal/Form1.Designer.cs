a033e0452ebc5065545ae67c2808274bf5b78862
Abrahao Bittar Silva <abrahaobittar@gmail.com>
2014-08-08 17:17:48 -0300

    Projeto com Visual Studio
    Calcula Indice Metabolico Basal

---
+++ FormPrincipal/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace FormPrincipal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.cbNivel = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tmb = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(78, 56);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(69, 20);
+            this.txtAltura.TabIndex = 0;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(78, 82);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(69, 20);
+            this.txtPeso.TabIndex = 1;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(78, 108);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(69, 20);
+            this.txtIdade.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Altura (CM)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Idade";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Peso (KG)";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(15, 161);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(279, 29);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbNivel
+            // 
+            this.cbNivel.FormattingEnabled = true;
+            this.cbNivel.Items.AddRange(new object[] {
+            "Sedentário",
+            "Pouco ativo ",
+            "Moderadamente ativo ",
+            "Muito ativo ",
+            "Extremamente ativo",
+            "            "});
+            this.cbNivel.Location = new System.Drawing.Point(92, 134);
+            this.cbNivel.Name = "cbNivel";
+            this.cbNivel.Size = new System.Drawing.Size(202, 21);
+            this.cbNivel.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Nivel Atividade";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.tmb);
+            this.panel1.Location = new System.Drawing.Point(163, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(131, 109);
+            this.panel1.TabIndex = 9;
+            // 
+            // tmb
+            // 
+            this.tmb.AutoSize = true;
+            this.tmb.Location = new System.Drawing.Point(3, 9);
+            this.tmb.Name = "tmb";
+            this.tmb.Size = new System.Drawing.Size(0, 13);
+            this.tmb.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 195);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbNivel);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ComboBox cbNivel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label tmb;
+    }
+}
+
---
+++ FormPrincipal/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormPrincipal
+{
+    public partial class Form1 : Form
+    {
+        string idade;
+        string altura;
+        string peso;
+        double total2 = 0;
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            idade = txtIdade.Text ;
+            altura = txtAltura.Text;
+            peso = txtPeso.Text;
+
+            double idadeD = Convert.ToDouble(idade);
+            double alturaD = Convert.ToDouble(altura);
+            double pesoD = Convert.ToDouble(peso);
+            
+
+            //Fórmula de Harris-Benedict
+            //Homens: 66,437 + (5,0033 x altura [cm]) + (13,7516 x peso [kg]) – (6,755 x idade [anos])
+            //1.2 = Sedentário (Pouco ou nenhum exercicio + desk job)
+            //1.3-1.4 =Pouco ativo (Pouco ativo no dia-a-dia & exercicios lights 1-3 dias por semana)
+            //1.5-1.6 = Moderadamente ativo (Moderadamente ativo no dia-a-dia & exercicios em intensidade moderada 3-5 dias por semana)
+            //1.7-1.8 = Muito ativo (Estilo de vida que exige esforco bruto & Exercicio intenso ou reliza algum esporte 6-7 dias por semana)
+            //1.9-2.0 = Extremamente ativo (Atividade de vida muito intensa ou esportes e trabalho fisico)
+            
+            double total = (66 + (5 * alturaD) + (13.7 * pesoD) - (6.8 * idadeD));
+
+            switch (Convert.ToString(cbNivel.SelectedIndex)) {
+                case "0":
+                    total2 = total * 1.2;
+                    break;
+                case "1" :
+                    total2 = total * 1.4;
+                    break;
+                case "2":
+                    total2 = total * 1.6;
+                    break;
+                case "3":
+                    total2 = total * 1.8;
+                    break;
+                case "4":
+                    total2 = total * 1.9;
+                    break;
+            } //fim switch
+
+            tmb.Text = Convert.ToString("Taxa Metabolica: "+total);
+            MessageBox.Show(Convert.ToString(total2));
+        }
+    }
+}
